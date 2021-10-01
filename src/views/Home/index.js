5b233a1eefb0f34cba62b65445dfed66f130bb6f
Ian-Bright <ianbrighton1@gmail.com>
2021-10-01 17:43:26 -0400

    save progress

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,9 +1,13 @@
 import { createUseStyles } from "react-jss";
+import { useState } from "react";
+import { useHistory } from "react-router";
 
 const useStyles = createUseStyles({
   cta: {
+    background: "#D4D4D4",
     border: "1px solid #959595",
     borderRadius: "3px",
+    cursor: "pointer",
     fontSize: "32px",
     fontWeight: 400,
     lineHeight: "38px",
@@ -25,6 +29,21 @@ const useStyles = createUseStyles({
     fontWeight: 600,
     lineHeight: "59px",
   },
+  input: {
+    width: "306px",
+  },
+  organization: {
+    fontSize: "24px",
+    fontWeight: 700,
+    lineHeight: "29px",
+  },
+  preplayMessage: {
+    fontSize: "24px",
+    fontWeight: 400,
+    lineHeight: "29px",
+    margin: "60px 0px",
+    maxWidth: "487px",
+  },
   wrapper: {
     alignItems: "center",
     display: "flex",
@@ -35,19 +54,41 @@ const useStyles = createUseStyles({
 });
 
 export default function Home() {
+  const history = useHistory();
+  const [play, setPlay] = useState(false);
+  const [text, setText] = useState("");
   const styles = useStyles();
   return (
     <div className={styles.wrapper}>
       <div className={styles.heading}>BATTLELINE</div>
       <div className={styles.description}>
-        A Battleship style game built using the Baseline Protocol
+        {play
+          ? "Your Team"
+          : "A Battleship style game built using the Baseline Protocol"}
       </div>
-      <div className={styles.ctaContainer}>
-        <div className={styles.cta} style={{ marginRight: "20px" }}>
-          Learn More
+      {play && (
+        <div className={styles.preplayMessage}>
+          Your team, or{" "}
+          <span className={styles.organization}>organization</span>, will be a
+          counterparty in the battlefield
         </div>
-        <div className={styles.cta}>Play</div>
-      </div>
+      )}
+      {!play ? (
+        <div className={styles.ctaContainer}>
+          <div className={styles.cta} style={{ marginRight: "20px" }}>
+            Learn More
+          </div>
+          <div className={styles.cta} style={{ width: "141px" }}>
+            Play
+          </div>
+        </div>
+      ) : (
+        <div style={{ display: "flex", maxWidth: "487px" }}>
+          <div className={styles.cta} onClick={() => setPlay(true)}>
+            Next
+          </div>
+        </div>
+      )}
     </div>
   );
 }
